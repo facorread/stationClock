148f3b7544e9eb2bb3416ee5d76830b97ca64c34
Fabio Correa <facorread@gmail.com>
2018-08-19 16:04:23 -0400

    Imported examClock with basic replacements

---
+++ README.md
@@ -0,0 +1,48 @@
+# lectureClock
+
+Simple clock and outline for classroom activities
+
+Copyright (C) 2016-2018 Fabio Correa fabio5@osu.edu
+
+https://github.com/facorread/lectureClock
+
+https://gitlab.com/facorread/lectureClock
+
+lectureClock is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+lectureClock is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with lectureClock.  If not, see <http://www.gnu.org/licenses/>.
+
+# User Guide
+
+Before students start coming into the classroom, turn on the computer and projector. In the web browser, go to http://facorread.github.io/lectureClock/ and type your messages, instructions, clarifications about the exam contents, specific questions, glossary, rules, etc.
+
+# Questions
+
+Q. How do I change the messages?
+
+A. Just click the text area and start typing.
+
+Q. How do I set the total exam time?
+
+A. Just click it and change it.
+
+Q. Why the boring style?
+
+A. White letters on a black background are easier on the eyes than black on white, particularly when looking at a screen or projection. Sans serif letters are easier to read, too. This project is designed to avoid distraction while presenting clear instructions and information.
+
+Q. How do I change the font sizes?
+
+A. Zoom in and out using your browser's controls.
+
+Q. How do I change the colors, fonts, or sizes? How can I move the clock and timer around?
+
+Use Chrome Developer Tools.
---
+++ script.js
@@ -0,0 +1,72 @@
+/* This file is part of lectureClock: Simple clock and outline for classroom activities
+Copyright (C) 2016-2018 Fabio Correa fabio5@osu.edu
+
+https://github.com/facorread/lectureClock
+https://gitlab.com/facorread/lectureClock
+
+lectureClock is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+lectureClock is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with lectureClock.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+// The exam time can be set by clicking it.
+var lectureTimeLeft = 75, lectureTimeEnabled = false;
+
+function init() {
+    renderTime();
+    renderLectureTimeLeft();
+    resizeExam();
+    setInterval(renderTime, 1000);
+}
+
+function renderTime() {
+    var d = new Date();
+    document.getElementById("currentTimeCell").innerHTML = d.toLocaleTimeString();
+    if(lectureTimeEnabled && (d.getSeconds() == 0)) {
+        --lectureTimeLeft;
+        renderLectureTimeLeft();
+    }
+}
+
+function renderLectureTimeLeft() {
+    var lectureTimeCell = document.getElementById("lectureTimeCell");
+    // The following should not be optimized because this function is invoked by setLectureTime() at any time.
+    if(lectureTimeEnabled) {
+        lectureTimeCell.innerHTML = "Time left: " + lectureTimeLeft + " Min";
+        if(lectureTimeLeft <= 10) {
+           lectureTimeCell.style.color = "red";
+        } else {
+            lectureTimeCell.style.color = document.body.style.color;
+        }
+    } else {
+        lectureTimeCell.innerHTML = "Click here for timer";
+        lectureTimeCell.style.color = document.body.style.color;
+    }
+}
+
+window.onload = init;
+
+function setLectureTime() {
+    var enteredText = prompt("Enter the new time left in minutes\n(0 to hide):", lectureTimeLeft);
+    if(enteredText.match(/^\d+$/)) {
+        lectureTimeEnabled = (enteredText > 0);
+        lectureTimeLeft = enteredText;
+    } else {
+        lectureTimeEnabled = false;
+    }
+    renderTime();
+    renderLectureTimeLeft();
+}
+
+function resizeExam() {
+    document.getElementById("ExamMessages").style.height = (window.innerHeight * 0.8) + "px";
+}
